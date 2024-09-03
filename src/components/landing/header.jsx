133a4945b471af59e851059aa1f4daaf5fceda32
Nnaemeka Joseph <nnaemekaxi@yahoo.com>
2024-09-03 14:51:46 +0100

    routes

--- src/components/landing/header.jsx
+++ src/components/landing/header.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import logo from '../../assets/images/logo.png'
 import styles from '../../assets/css/landing/styles.module.css'
 
 function Header() {
   return (
     <div className={styles.Header}>
-        <section className={styles.HeaderLogo}>
+        <Link to="/"><section className={styles.HeaderLogo}>
             <img src={logo} alt="logo" style={{height: '30px'}}/>
             <h3>BookStore</h3>
-        </section>
+        </section></Link>
         <section className={styles.HeaderSearch}>
             <input type="search" name="" id="" placeholder='Search Titles, Authors, Genres' className={styles.Search} />
         </section>
         <section className={styles.HeaderAuth}>
-            <button className={styles.Register}>Register</button>
-            <button className={styles.Login}>Login</button>
+        <Link to="/auth/register"><button className={styles.Register}>Register </button></Link>
+          <Link to="/auth/login"><button className={styles.Login}>Login</button></Link>
         </section>
     </div>
   )
